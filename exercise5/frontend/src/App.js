e63109dd12eb1da25b99f933868c1a7dd28e00be
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2022-02-18 11:41:28 +0200

    adminmode button and functionality added

--- exercise5/frontend/src/App.js
+++ exercise5/frontend/src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-//import productList from './data/products'
 import { useState, useEffect } from 'react'
 import Products from './components/Products'
 import SearchBar from './components/SearchBar';
 import axios from 'axios'
+import styles from './styles.module.css'
+import EditorView from './components/EditorView'
 
 function App() {
 
   const [products, setProducts] = useState([])
   const [filtered, setFiltered] = useState([])
   const [noResults, setNoResults] = useState(false)
+  const [adminMode, setAdminMode] = useState(false)
+
   const locationInfo = "Ships to Finland"
 
   useEffect(() => {
-    /*
-      setProducts(productList.products)
-      setFiltered(productList.products)
-    */
     const getData = async () => {
       const results = await axios.get('http://localhost:3001/products')
       console.log(results.data);
@@ -39,7 +38,6 @@ function App() {
   }, [filtered])
 
     const filteredData = (search) => {
-
       if (search !== '') {
         setFiltered(() => products.filter(prod => 
         prod.description.toLowerCase().includes(search.toLowerCase())
@@ -48,13 +46,15 @@ function App() {
         setFiltered(products)
       }
     }
+  
 
   return (
     <div className="App">
       <h1 className="header">Welcome to the webshop</h1>
+      <button className={styles.button} onClick={() => setAdminMode(!adminMode)}>Admin Mode</button>
       <SearchBar filter={ filteredData }/>
       { noResults === true ? <h3 className="header">No Results</h3>: <h3 className="header">Results</h3>}
-      <Products data={ filtered } location={locationInfo}/>
+      { adminMode === true ? <EditorView /> : <Products data={ filtered } location={locationInfo}/>}
     </div>
   );
 }
